00cfe64218c65642a0d410725e2f46783c43ab58
Timo <timojenkins21@gmail.com>
2020-02-05 11:20:14 +0100

    added method multiply

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,11 +9,18 @@ public class Main {
         double a = 33.3;
         double b = 66.6;
         double r = sum(a, b);
+        double r2 = multiply(a, b);
         System.out.println(a + " + " + b + " = " + r);
-
+        System.out.println(a + " * " + b + " = " + r2);
     }
 
     public static double sum(double a, double b){
         return a + b;
     }
+
+    public static double multiply(double a, double b){
+        return a * b;
+    }
+
+
 }
